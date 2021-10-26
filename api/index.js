15618e6c22545ca8a8b9cdebcc565404a7f1ab97
mohamad <msiysinyuy@gmail.com>
2021-10-26 14:21:06 +0100

    the blog backend complete

--- api/index.js
+++ api/index.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose")
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const postRoute = require('./routes/posts')
+const catRoute = require('./routes/categories')
+const multer = require('multer')
+
+
 const app = express();
 
 
@@ -16,10 +20,25 @@ mongoose.connect(process.env.MONGO_URI)
 .then(console.log("connected to MongoDB"))
 .catch(error=>console.log(error))
 
+
+//image storage
+  const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+      cb(null,"images")
+    },
+    filename:(req,file,cb) =>{cb(null,req.body.name)}
+  })
+
+  const upload = multer({storage:storage});
+  app.post("/api/upload",upload.single("file"),(req,res) =>{
+     res.status(200).json("File hasbeen uploaded")
+  })
 //routes
 app.use("/api/auth",authRoute)
 app.use("/api/user",userRoute)
 app.use("/api/post",postRoute)
+app.use("/api/categories",catRoute)
+
 
 
 
